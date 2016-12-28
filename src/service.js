875c3af52172054453e380d30bc85f2bf94973d2
kwin <kwin.xu@163.com>
2016-12-28 17:43:56 +0800

    add

--- src/service.js
+++ src/service.js
@@ -11,7 +11,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ServiceSchema = new Schema({
-    serviceCore: { type: Schema.ObjectId, ref: 'ServiceCore' }
+    serviceCore: { type: Schema.ObjectId, ref: 'ServiceCore' },
+    target: { type: Schema.ObjectId, ref: 'User' },
+    type: { type: String, enum: ['PLATFORM', 'DOCTOR', 'CLIENT'] },
+    
 });
 
 
--- src/service_core.js
+++ src/service_core.js
@@ -13,10 +13,11 @@ const Schema = mongoose.Schema;
 const ServiceCoreSchema = new Schema({
     name: { type: String, required: true },                     //服务名称
     price: { type: Number, required: true },                    //服务价格
-    nurse_period: { type: Number, required: true },             //看护时间
+    nurse_period: [{ type: Number, required: true }],           //看护时间
     modules: [{ type: Schema.ObjectId, ref: 'NurseModule' }],   //检测项  看护模块
+    creator: { type: Schema.ObjectId, ref: 'User' },            //创建者
     description: { type: String },                              //服务描述
-    status: { type: String, default: '0' },
+    status: { type: String, default: '0' },                     //服务状态{'0': '未完成', '1': '已完成', '2': '已下架'}
     create_at: { type: Date, default: Date.now },               //创建时间
     update_at: { type: Date, default: Date.now }                //修改时间
 });
