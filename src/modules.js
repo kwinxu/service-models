15f48cd4f39286661b62ec4135fde25caf59f395
kwin <kwin.xu@163.com>
2016-12-29 22:32:01 +0800

    add

---
+++ src/modules.js
@@ -0,0 +1,17 @@
+/**
+ *@author xyx <kwin.xu@semioe.com>
+ *        date: 2016.12.29
+ *@description 
+ */
+/* jshint ignore:start */
+'use strict';
+/* jshint ignore:end   */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ModulesSchema = new Schema({
+    
+});
+
+module.exports = ModulesSchema;
