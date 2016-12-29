f0da496e881983b532613d1f6b956da19a90272b
kwin <kwin.xu@163.com>
2016-12-29 22:51:24 +0800

    updatre

--- src/module.js
+++ src/module.js
@@ -10,13 +10,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const NurseModuleSchema = new Schema({
+const ModuleSchema = new Schema({
     key: { type: String, required: true },                                              //key
     label: { type: String, required: true },                                            //显示名称
-    type: { type: String, enum: ['nurse','remind'] },                                   //模块类型
+    type: { type: String, required: true, enum: ['nurse', 'remind'] },                  //模块类型
     description: { type: String },                                                      //检测项木块介绍
     create_at: { type: Date, default: Date.now },                                       //创建时间
     update_at: { type: Date, default: Date.now }                                        //修改时间
 });
 
-module.exports = NurseModuleSchema;
+module.exports = ModuleSchema;
